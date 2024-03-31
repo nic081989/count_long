45c8ce118939e14278e707743142723dfbcd6622
nic081989 <93353802+nic081989@users.noreply.github.com>
2024-03-30 23:21:01 -0400

    Add files via upload

---
+++ count_longest.py
@@ -0,0 +1,36 @@
+# MODIFY ME TO IMPLEMENT YOUR SOLUTION
+# TO PROBLEM 4: COUNT THE LONGEST SUBSEQUENCE
+#
+# NAME:         Nicholas Ngobi
+# ASSIGNMENT:   Project 2: Stacks & Queues
+
+from Queue import Queue
+
+# count longest sequence of duplicates in a queue
+# can destroy the queue & make it empty
+def count_longest(q):
+    current_length = 1  # each starting character in a given string starts 1
+    longLength = 0 # long length is initialized as 0 bc it's not yet known/ may as well be empty
+    while not q.is_empty():# Condition to check or not the queue is empty
+        current = q.deq() #Removes the front and stores it 
+        while current == q.front(): # Removed/dequeued matches the new front of q/ this is acondition for values repeating
+                                    # while true /once current != to the new front, the while loop will stop.
+            q.deq()                 # dequeuing the q/ this will occur regardless 
+            current_length += 1     # Increaing the current length
+        if current_length > longLength:
+            longLength = current_length
+        current_length = 1          # reset back to 1 as we go to new character.
+    return longLength
+
+def main():
+
+    print(count_longest(Queue([l for l in "oooohheeee"])) == 4)
+    print("out 2:", count_longest( Queue( [ l for l in "hello" ] ) ))
+    print("out 5:", count_longest(Queue([l for l in "m" * 5])))
+    print("out 3:", count_longest(Queue([l for l in "heee"])))
+
+
+# Don't run main on import
+if __name__ == "__main__":
+    main()
+
